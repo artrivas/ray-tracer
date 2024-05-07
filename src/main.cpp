b7c5b93eb174ab3bf873956010c5d2e52e5b72dc
artrivas <rivaschavezd5k@gmail.com>
2024-05-07 14:30:08 -0500

    Antialiasing

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,8 @@ int main() {
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
     cam.image_width = 400;
-
+    cam.samples_per_pixel = 100;
+    
     cam.render(world);
     return 0;
 }
--- src/rtweekend.h
+++ src/rtweekend.h
@@ -6,6 +6,7 @@
 #define RAYTRACING_WEEKEND_RTWEEKEND_H
 
 #include <cmath>
+#include <cstdlib>
 #include <iostream>
 #include <limits>
 #include <memory>
@@ -25,6 +26,17 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+
+inline double random_double() {
+    // Returns a random real in [0,1).
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max-min)*random_double();
 }
 
 // Common Headers
