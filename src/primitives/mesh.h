cdb58d21681a4a139645523b72cf9a296a2e7394
juaquin456 <juaquin.remon@utec.edu.pe>
2024-05-08 17:44:36 -0500

    Update: reformat mesh class

--- src/primitives/mesh.h
+++ src/primitives/mesh.h
@@ -16,6 +16,11 @@
 
 class mesh : public hittable {
 private:
+    shared_ptr<material> mat = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+    point3 origin{0, 0, 0};
+    tinyobj::attrib_t attrib;
+    std::vector<tinyobj::shape_t> shapes;
+    std::vector<tinyobj::material_t> materials;
 public:
     mesh() = default;
 
@@ -23,18 +28,18 @@ public:
         bool intersects = false;
 
         for (int i = 0; i < this->shapes[0].mesh.indices.size() / 3; i++) {
-            int idx0 = shapes[0].mesh.indices.at(3 * i).vertex_index;
-            int idx1 = shapes[0].mesh.indices.at(3 * i + 1).vertex_index;
-            int idx2 = shapes[0].mesh.indices.at(3 * i + 2).vertex_index;
+            const int &idx0 = shapes[0].mesh.indices.at(3 * i).vertex_index;
+            const int &idx1 = shapes[0].mesh.indices.at(3 * i + 1).vertex_index;
+            const int &idx2 = shapes[0].mesh.indices.at(3 * i + 2).vertex_index;
 
-            point3 vtx1{attrib.vertices.at(3 * idx0), attrib.vertices.at(3 * idx0 + 1),
-                        attrib.vertices.at(3 * idx0 + 2)};
-            point3 vtx2{attrib.vertices.at(3 * idx1), attrib.vertices.at(3 * idx1 + 1),
-                        attrib.vertices.at(3 * idx1 + 2)};
-            point3 vtx3{attrib.vertices.at(3 * idx2), attrib.vertices.at(3 * idx2 + 1),
-                        attrib.vertices.at(3 * idx2 + 2)};
+            const point3 vtx1{attrib.vertices.at(3 * idx0), attrib.vertices.at(3 * idx0 + 1),
+                              attrib.vertices.at(3 * idx0 + 2)};
+            const point3 vtx2{attrib.vertices.at(3 * idx1), attrib.vertices.at(3 * idx1 + 1),
+                              attrib.vertices.at(3 * idx1 + 2)};
+            const point3 vtx3{attrib.vertices.at(3 * idx2), attrib.vertices.at(3 * idx2 + 1),
+                              attrib.vertices.at(3 * idx2 + 2)};
 
-            triangle t(vtx1,vtx2,vtx3, this->mat);
+            const triangle t(vtx1, vtx2, vtx3, this->mat);
 
             if (t.hit(r, ray_t, rec)) {
                 intersects = true;
@@ -47,11 +52,11 @@ public:
         return this->origin;
     }
 
-    void set_origin(const point3& _origin) {
-        for (int i = 0; i < this->attrib.vertices.size()/3; i++) {
-            this->attrib.vertices.at(3*i + 0) += _origin.x() - this->origin.x();
-            this->attrib.vertices.at(3*i + 1) += _origin.y() - this->origin.y();
-            this->attrib.vertices.at(3*i + 2) += _origin.z() - this->origin.z();
+    void set_origin(const point3 &_origin) {
+        for (int i = 0; i < this->attrib.vertices.size() / 3; i++) {
+            this->attrib.vertices.at(3 * i + 0) += _origin.x() - this->origin.x();
+            this->attrib.vertices.at(3 * i + 1) += _origin.y() - this->origin.y();
+            this->attrib.vertices.at(3 * i + 2) += _origin.z() - this->origin.z();
         }
         this->origin = _origin;
     }
@@ -69,12 +74,6 @@ public:
 
         return std::move(new_mesh);
     }
-
-    shared_ptr<material> mat = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    point3 origin{0, 0, 0};
-    tinyobj::attrib_t attrib;
-    std::vector<tinyobj::shape_t> shapes;
-    std::vector<tinyobj::material_t> materials;
 };
 
 #endif //RAYTRACING_WEEKEND_MESH_H
