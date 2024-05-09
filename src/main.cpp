665272b40acf731233ae0173f9780d045977a9fd
Nkon-522 <nkon.cs@gmail.com>
2024-05-08 20:58:17 -0500

    Add - human obj

--- src/main.cpp
+++ src/main.cpp
@@ -10,22 +10,22 @@ int main() {
     // World
     hittable_list world;
     auto t = mesh::build("../samples/dodecahedron.obj");
-    t.set_origin({0, 0, -2});
+    t.set_origin({-1.0, 0, -2});
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
+//    auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
 //    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
 //    auto material_left = make_shared<dielectric>(1.00/1.33);
     auto material_left   = make_shared<dielectric>(1.50);
-    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
+//    auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
+//    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 1.0);
 
-    //auto material_triangle = make_shared<metal>(color(1, 0.2, 0.2), 0.0);
+    auto material_metal = make_shared<metal>(color(1, 0.2, 0.2), 0.0);
 
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+//    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
+//    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+//    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_metal));
     world.add(make_shared<mesh>(t));
     //   world.add(make_shared<triangle>(point3(-0.5, -0.5, -0.6), point3(0.5, -0.5, -0.6), point3(0, 1, -0.6)));
     //   world.add(make_shared<triangle>(point3(-3, 0, -3), point3(3, 0, -3), point3(0, 2, -2),material_triangle));
