0e47d706046789f6eac5a81a66f99e26299368bb
juaquin456 <juaquin.remon@utec.edu.pe>
2024-05-09 00:55:36 -0500

    Add: BVH tree in mesh

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,7 @@
 int main() {
     // World
     hittable_list world;
-    auto t = mesh::build("../samples/dodecahedron.obj");
-    t.set_origin({0, 0, -2});
+    auto t = mesh::build("../samples/airboat.obj", {1.5, 0, -7});
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
     auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8), 0.3);
@@ -18,10 +17,10 @@ int main() {
 
     auto material_triangle = make_shared<metal>(color(1, 0.2, 0.2), 0.0);
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
+//    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     //world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+//    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
+//    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
     world.add(make_shared<mesh>(t));
     //   world.add(make_shared<triangle>(point3(-0.5, -0.5, -0.6), point3(0.5, -0.5, -0.6), point3(0, 1, -0.6)));
 //    world.add(make_shared<triangle>(point3(-3, 0, -3), point3(3, 0, -3), point3(0, 2, -2),material_triangle));
@@ -30,7 +29,7 @@ int main() {
     // Camera
     camera cam;
     cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 1000;
+    cam.image_width = 400;
     cam.samples_per_pixel = 100;
     cam.max_depth = 50;
     
--- src/primitives/mesh.h
+++ src/primitives/mesh.h
@@ -13,6 +13,35 @@
 #include <vector>
 #include "../ext/tiny_obj_loader/tiny_obj_loader.h"
 #include "../primitives/triangle.h"
+#include "../material/material.h"
+#include "bvh/v2/bvh.h"
+#include "bvh/v2/executor.h"
+#include "bvh/v2/thread_pool.h"
+#include "bvh/v2/bbox.h"
+#include "bvh/v2/tri.h"
+#include "bvh/v2/default_builder.h"
+#include "bvh/v2/stack.h"
+
+using Scalar = float;
+using Vec3 = bvh::v2::Vec<Scalar, 3>;
+using BBox = bvh::v2::BBox<Scalar, 3>;
+using Tri = bvh::v2::Tri<Scalar, 3>;
+using Node = bvh::v2::Node<Scalar, 3>;
+using Bvh = bvh::v2::Bvh<Node>;
+using Ray = bvh::v2::Ray<Scalar, 3>;
+
+
+using PrecomputedTri = bvh::v2::PrecomputedTri<Scalar>;
+
+namespace convert {
+    Vec3 to(const vec3 &v) {
+        return {static_cast<float>(v.x()), static_cast<float>(v.y()), v.z()};
+    }
+
+    vec3 from(const Vec3 &v) {
+        return {v.values[0], v.values[1], v.values[2]};
+    }
+}
 
 class mesh : public hittable {
 private:
@@ -21,60 +50,127 @@ private:
     tinyobj::attrib_t attrib;
     std::vector<tinyobj::shape_t> shapes;
     std::vector<tinyobj::material_t> materials;
+
+    // BVH
+    shared_ptr<Bvh> bvh;
+    std::vector<PrecomputedTri> precomputed;
 public:
     mesh() = default;
 
-    bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
-        hit_record temp_rec;
-        bool intersects = false;
-        auto closest_so_far = ray_t.max;
-        for (int i = 0; i < this->shapes[0].mesh.indices.size() / 3; i++) {
-            const int &idx0 = shapes[0].mesh.indices.at(3 * i).vertex_index;
-            const int &idx1 = shapes[0].mesh.indices.at(3 * i + 1).vertex_index;
-            const int &idx2 = shapes[0].mesh.indices.at(3 * i + 2).vertex_index;
-
-            const point3 vtx1{attrib.vertices.at(3 * idx0), attrib.vertices.at(3 * idx0 + 1),
-                              attrib.vertices.at(3 * idx0 + 2)};
-            const point3 vtx2{attrib.vertices.at(3 * idx1), attrib.vertices.at(3 * idx1 + 1),
-                              attrib.vertices.at(3 * idx1 + 2)};
-            const point3 vtx3{attrib.vertices.at(3 * idx2), attrib.vertices.at(3 * idx2 + 1),
-                              attrib.vertices.at(3 * idx2 + 2)};
-
-            const triangle t(vtx1, vtx2, vtx3, this->mat);
-
-            if (t.hit(r, interval(ray_t.min, closest_so_far), temp_rec)) {
-                intersects = true;
-                closest_so_far = temp_rec.t;
-                rec = temp_rec;
+    void for_each(auto fn) {
+        for (auto& shape : shapes) {
+            for (int i = 0; i < shape.mesh.indices.size() / 3; i++) {
+                const int &idx0 = shape.mesh.indices.at(3 * i).vertex_index;
+                const int &idx1 = shape.mesh.indices.at(3 * i + 1).vertex_index;
+                const int &idx2 = shape.mesh.indices.at(3 * i + 2).vertex_index;
+                point3 vtx1{attrib.vertices.at(3 * idx0), attrib.vertices.at(3 * idx0 + 1),
+                            attrib.vertices.at(3 * idx0 + 2)};
+                point3 vtx2{attrib.vertices.at(3 * idx1), attrib.vertices.at(3 * idx1 + 1),
+                            attrib.vertices.at(3 * idx1 + 2)};
+                point3 vtx3{attrib.vertices.at(3 * idx2), attrib.vertices.at(3 * idx2 + 1),
+                            attrib.vertices.at(3 * idx2 + 2)};
+                vtx1 += origin;
+                vtx2 += origin;
+                vtx3 += origin;
+                fn(vtx1, vtx2, vtx3);
             }
         }
-        return intersects;
     }
 
-    [[nodiscard]] point3 get_origin() const {
-        return this->origin;
+    bool hit(const ray &r, interval ray_t, hit_record &rec) const override {
+        auto ray = Ray{
+                convert::to(r.origin()),
+                convert::to(r.direction()),
+                static_cast<float>(ray_t.min),
+                static_cast<float>(ray_t.max),
+        };
+
+        static constexpr size_t invalid_id = std::numeric_limits<size_t>::max();
+        static constexpr size_t stack_size = 64;
+        static constexpr bool use_robust_traversal = false;
+
+        auto prim_id = invalid_id;
+        Scalar u, v;
+        static constexpr bool should_permute = true;
+
+        bvh::v2::SmallStack<Bvh::Index, stack_size> stack;
+        bvh->intersect<false, use_robust_traversal>(ray, bvh->get_root().index, stack,
+                                                   [&](size_t begin, size_t end) {
+                                                       for (size_t i = begin; i < end; ++i) {
+                                                           size_t j = should_permute ? i : bvh->prim_ids[i];
+                                                           if (auto hit = precomputed[j].intersect(ray)) {
+                                                               prim_id = i;
+                                                               std::tie(u, v) = *hit;
+                                                           }
+                                                       }
+                                                       return prim_id != invalid_id;
+                                                   });
+
+        if (prim_id != invalid_id) {
+            auto t = precomputed.at(prim_id);
+            rec.t = ray.tmax;
+            rec.mat = this->mat;
+            rec.p = r.at(rec.t);
+            vec3 outward_normal = convert::from(normalize(cross(t.e1, t.e2)));
+            rec.set_face_normal(r, outward_normal);
+            return true;
+        } else return false;
     }
 
-    void set_origin(const point3 &_origin) {
-        for (int i = 0; i < this->attrib.vertices.size() / 3; i++) {
-            this->attrib.vertices.at(3 * i + 0) += _origin.x() - this->origin.x();
-            this->attrib.vertices.at(3 * i + 1) += _origin.y() - this->origin.y();
-            this->attrib.vertices.at(3 * i + 2) += _origin.z() - this->origin.z();
-        }
-        this->origin = _origin;
-    }
-
-    static mesh build(const std::string &filename) {
+    static mesh build(const std::string &filename, const point3& origin) {
         mesh new_mesh;
-        std::string err;
-        bool status = tinyobj::LoadObj(&new_mesh.attrib, &new_mesh.shapes, &new_mesh.materials, nullptr, &err, filename
-                .data());
-        if (!err.empty()) {
-            std::cerr << "Failed to load: " << err << std::endl;
-            exit(1);
+        new_mesh.origin = origin;
+        {
+            // READ OBJ FILE
+            std::string err;
+            bool status = tinyobj::LoadObj(&new_mesh.attrib, &new_mesh.shapes, &new_mesh.materials, nullptr, &err,
+                                           filename
+                                                   .data());
+            if (!err.empty()) {
+                std::cerr << "Failed to load: " << err << std::endl;
+                exit(1);
+            }
+            if (!status) exit(1);
+            std::clog << "shapes: " << new_mesh.shapes.size() << std::endl;
+            std::clog << "Mesh loaded: " << new_mesh.shapes[0].name << " - " << new_mesh.attrib.vertices.size() <<
+            std::endl;
         }
-        if (!status) exit(1);
 
+        std::vector<Tri> tris;
+        auto lm = [&tris](const point3 &vtx1, const point3 &vtx2, const point3 &vtx3) {
+            tris.emplace_back(Vec3(vtx1.x(), vtx1.y(), vtx1.z()), Vec3(vtx2.x(), vtx2.y(), vtx2.z()), Vec3(vtx3.x(),
+                                                                                                           vtx3.y(),
+                                                                                                           vtx3.z()));
+        };
+        // fill tris with the data
+        new_mesh.for_each(lm);
+
+        bvh::v2::ThreadPool thread_pool;
+        bvh::v2::ParallelExecutor executor(thread_pool);
+
+        std::vector<BBox> bboxes(tris.size());
+        std::vector<Vec3> centers(tris.size());
+
+        executor.for_each(0, tris.size(), [&](size_t begin, size_t end) {
+            for (size_t i = begin; i < end; ++i) {
+                bboxes[i] = tris[i].get_bbox();
+                centers[i] = tris[i].get_center();
+            }
+        });
+
+        typename bvh::v2::DefaultBuilder<Node>::Config config;
+        config.quality = bvh::v2::DefaultBuilder<Node>::Quality::High;
+        new_mesh.bvh = make_shared<Bvh>(bvh::v2::DefaultBuilder<Node>::build(thread_pool, bboxes, centers, config));
+
+        static constexpr bool should_permute = true;
+
+        new_mesh.precomputed.resize(tris.size());
+        executor.for_each(0, tris.size(), [&](size_t begin, size_t end) {
+            for (size_t i = begin; i < end; ++i) {
+                auto j = should_permute ? new_mesh.bvh->prim_ids[i] : i;
+                new_mesh.precomputed[i] = tris[j];
+            }
+        });
         return std::move(new_mesh);
     }
 };
