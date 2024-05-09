82913466e97541b7d853e490710c723822f5187b
Nkon-522 <nkon.cs@gmail.com>
2024-05-09 00:56:36 -0500

    Add - lenses

--- src/main.cpp
+++ src/main.cpp
@@ -55,18 +55,18 @@ int main() {
 
     // Camera
     camera cam;
-    cam.aspect_ratio = 16.0 / 9.0;
-    cam.image_width = 1000;
-    cam.samples_per_pixel = 100;
-    cam.max_depth = 50;
+    cam.aspect_ratio      = 16.0 / 9.0;
+    cam.image_width       = 1000;
+    cam.samples_per_pixel = 500;
+    cam.max_depth         = 50;
 
-    cam.vfov = 90;
-    cam.lookfrom = point3(-2,2,1);
-    cam.lookat   = point3(0,0,-1);
+    cam.vfov     = 20;
+    cam.lookfrom = point3(13,2,3);
+    cam.lookat   = point3(0,0,0);
     cam.vup      = vec3(0,1,0);
 
-    cam.defocus_angle = 10.0;
-    cam.focus_dist    = 3.4;
+    cam.defocus_angle = 0.6;
+    cam.focus_dist    = 10.0;
 
     cam.render(world);
     return 0;
