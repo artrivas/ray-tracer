18241631e6ec6c85388b2b6b06fd6a882cf90bcb
artrivas <rivaschavezd5k@gmail.com>
2024-05-15 20:15:59 -0500

    Motion Blur

--- src/camera/camera.h
+++ src/camera/camera.h
@@ -128,7 +128,10 @@ private:
         auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
-        return ray(ray_origin, ray_direction);
+        auto ray_time = random_double(); // generar un tiempo aleatorio entre [0,1)
+
+
+        return ray(ray_origin, ray_direction,ray_time);
     }
 
     vec3 sample_square() const {
