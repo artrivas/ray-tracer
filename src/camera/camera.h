c7ef3992eddbacbf595490798fbb4031950a8010
edededd <luis.chahua@utec.edu.pe>
2024-05-28 12:47:26 -0400

    montecarlo approach initialized

--- src/camera/camera.h
+++ src/camera/camera.h
@@ -14,46 +14,54 @@
 #include <omp.h>
 #include <vector>
 
-class camera {
+class camera
+{
 public:
     double aspect_ratio = 1.0;  // Ratio of image width over height
-    int    image_width  = 100;  // Rendered image width in pixel count
-    int    samples_per_pixel = 10;   // Count of random samples for each pixel
-    int    max_depth = 10;  // Maximum number of ray bounces into scene
-    color  background;               // Scene background color
+    int image_width = 100;      // Rendered image width in pixel count
+    int samples_per_pixel = 10; // Count of random samples for each pixel
+    int max_depth = 10;         // Maximum number of ray bounces into scene
+    color background;           // Scene background color
 
-    double vfov = 90;  // Vertical view angle (field of view)
-    point3 lookfrom = point3(0,0,0);   // Point camera is looking from
-    point3 lookat   = point3(0,0,-1);  // Point camera is looking at
-    vec3   vup      = vec3(0,1,0);     // Camera-relative "up" direction
+    double vfov = 90;                  // Vertical view angle (field of view)
+    point3 lookfrom = point3(0, 0, 0); // Point camera is looking from
+    point3 lookat = point3(0, 0, -1);  // Point camera is looking at
+    vec3 vup = vec3(0, 1, 0);          // Camera-relative "up" direction
 
-    double defocus_angle = 0;  // Variation angle of rays through each pixel
-    double focus_dist = 10;    // Distance from camera lookfrom point to plane of perfect focus
+    double defocus_angle = 0; // Variation angle of rays through each pixel
+    double focus_dist = 10;   // Distance from camera lookfrom point to plane of perfect focus
 
-    void render(hittable& world) {
+    void render(hittable &world)
+    {
         initialize();
 
         // Render
         std::vector<std::vector<vec3>> image(image_height, std::vector<vec3>(image_width));
 
-        for (int j = 0; j <image_height; ++j) {
-            for (int i = 0; i < image_width; ++i) {
-                color pixel_color(0,0,0);
-                for (int sample = 0; sample < samples_per_pixel; sample++) {
+        for (int j = 0; j < image_height; ++j)
+        {
+            for (int i = 0; i < image_width; ++i)
+            {
+                color pixel_color(0, 0, 0);
+                for (int sample = 0; sample < samples_per_pixel; sample++)
+                {
                     ray r = get_ray(i, j);
                     pixel_color += ray_color(r, max_depth, world);
                 }
                 image[j][i] = pixel_samples_scale * pixel_color;
             }
         }
-        std::clog<<"\rDone.        \n";
+        std::clog << "\rDone.        \n";
 
         // Save image
         file = std::ofstream("image.ppm");
-        file << "P3\n" << image_width << ' '<< image_height<<"\n255\n";
+        file << "P3\n"
+             << image_width << ' ' << image_height << "\n255\n";
 
-        for (int j = 0; j < image_height; j++) {
-            for (int i = 0; i < image_width; i++) {
+        for (int j = 0; j < image_height; j++)
+        {
+            for (int i = 0; i < image_width; i++)
+            {
                 write_color(file, image[j][i]);
             }
         }
@@ -61,36 +69,69 @@ public:
         file.close();
     }
 
+    void render_montecarlo(hittable &world)
+    {
+        initialize();
+
+        std::cout << "P3\n"
+                  << image_width << ' ' << image_height << "\n255\n";
+
+        for (int j = 0; j < image_height; j++)
+        {
+            std::clog << "\rScanlines remaining: " << (image_height - j) << ' ' << std::flush;
+            for (int i = 0; i < image_width; i++)
+            {
+                color pixel_color(0, 0, 0);
+                for (int s_j = 0; s_j < sqrt_spp; s_j++)
+                {
+                    for (int s_i = 0; s_i < sqrt_spp; s_i++)
+                    {
+                        ray r = get_ray_montecarlo(i, j, s_i, s_j);
+                        pixel_color += ray_color(r, max_depth, world);
+                    }
+                }
+                write_color(std::cout, pixel_samples_scale * pixel_color);
+            }
+        }
+
+        std::clog << "\rDone.                 \n";
+    }
+
 private:
     // file output
     std::ofstream file;
 
-    int    image_height;   // Rendered image height
-    double pixel_samples_scale;  // Color scale factor for a sum of pixel samples
-    point3 center;         // Camera center
-    point3 pixel00_loc;    // Location of pixel 0, 0
-    vec3   pixel_delta_u;  // Offset to pixel to the right
-    vec3   pixel_delta_v;  // Offset to pixel below
-    vec3   u, v, w;              // Camera frame basis vectors
-    vec3   defocus_disk_u;       // Defocus disk horizontal radius
-    vec3   defocus_disk_v;       // Defocus disk vertical radius
+    int image_height;           // Rendered image height
+    double pixel_samples_scale; // Color scale factor for a sum of pixel samples
+    point3 center;              // Camera center
+    point3 pixel00_loc;         // Location of pixel 0, 0
+    vec3 pixel_delta_u;         // Offset to pixel to the right
+    vec3 pixel_delta_v;         // Offset to pixel below
+    vec3 u, v, w;               // Camera frame basis vectors
+    vec3 defocus_disk_u;        // Defocus disk horizontal radius
+    vec3 defocus_disk_v;        // Defocus disk vertical radius
+    int sqrt_spp;               // Square root of number of samples per pixel
+    double recip_sqrt_spp;      // 1 / sqrt_spp
 
-    void initialize() {
+    void initialize()
+    {
         // Calculate the image height, and ensure that it's at least 1.
         image_height = int(image_width / aspect_ratio);
         image_height = (image_height < 1) ? 1 : image_height;
-
+        sqrt_spp = int(sqrt(samples_per_pixel));
+        pixel_samples_scale = 1.0 / (sqrt_spp * sqrt_spp);
+        recip_sqrt_spp = 1.0 / sqrt_spp;
         pixel_samples_scale = 1.0 / samples_per_pixel;
 
         center = lookfrom;
 
         // Determine viewport dimensions
         auto theta = degrees_to_radians(vfov);
-        auto h = tan(theta/2);
+        auto h = tan(theta / 2);
 
         // Viewport width < 1 ok since they are in a continuous space
         auto viewport_height = 2 * h * focus_dist;
-        auto viewport_width = viewport_height * (double(image_width)/image_height);
+        auto viewport_width = viewport_height * (double(image_width) / image_height);
 
         // Calculate the u,v,w unit basis vectors for the camera coordinate frame.
         w = unit_vector(lookfrom - lookat);
@@ -98,15 +139,15 @@ private:
         v = cross(w, u);
 
         // Calculate the vectors across the horizontal and down the vertical viewport edges.
-        vec3 viewport_u = viewport_width * u;    // Vector across viewport horizontal edge
-        vec3 viewport_v = viewport_height * -v;  // Vector down viewport vertical edge
+        vec3 viewport_u = viewport_width * u;   // Vector across viewport horizontal edge
+        vec3 viewport_v = viewport_height * -v; // Vector down viewport vertical edge
 
         // Calculate the horizontal and vertical delta vectors from pixel to pixel.
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate the location of the upper left pixel.
-        auto viewport_upper_left = center - (focus_dist * w) - viewport_u/2 - viewport_v/2;
+        auto viewport_upper_left = center - (focus_dist * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
         // Calculate the camera defocus disk basis vectors.
@@ -115,39 +156,66 @@ private:
         defocus_disk_v = v * defocus_radius;
     }
 
-    ray get_ray(int i, int j) const {
+    ray get_ray(int i, int j) const
+    {
         // Construct a camera ray originating from the defocus disk and directed at a randomly
         // sampled point around the pixel location i, j.
 
         auto offset = sample_square();
-        auto pixel_sample = pixel00_loc
-                          + ((i + offset.x()) * pixel_delta_u)
-                          + ((j + offset.y()) * pixel_delta_v);
+        auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
 
         auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
         auto ray_direction = pixel_sample - ray_origin;
 
         auto ray_time = random_double(); // generar un tiempo aleatorio entre [0,1)
 
-
-        return ray(ray_origin, ray_direction,ray_time);
+        return ray(ray_origin, ray_direction, ray_time);
     }
 
-    vec3 sample_square() const {
+    ray get_ray_montecarlo(int i, int j, int s_i, int s_j) const
+    {
+        // Construct a camera ray originating from the defocus disk and directed at a randomly
+        // sampled point around the pixel location i, j for stratified sample square s_i, s_j.
+
+        auto offset = sample_square_stratified(s_i, s_j);
+        auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
+
+        auto ray_origin = (defocus_angle <= 0) ? center : defocus_disk_sample();
+        auto ray_direction = pixel_sample - ray_origin;
+        auto ray_time = random_double();
+
+        return ray(ray_origin, ray_direction, ray_time);
+    }
+    vec3 sample_square_stratified(int s_i, int s_j) const
+    {
+        // Returns the vector to a random point in the square sub-pixel specified by grid
+        // indices s_i and s_j, for an idealized unit square pixel [-.5,-.5] to [+.5,+.5].
+
+        auto px = ((s_i + random_double()) * recip_sqrt_spp) - 0.5;
+        auto py = ((s_j + random_double()) * recip_sqrt_spp) - 0.5;
+
+        return vec3(px, py, 0);
+    }
+
+    vec3 sample_square() const
+    {
         // Returns the vector to a random point in the [-.5,-.5]-[+.5,+.5] unit square.
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    point3 defocus_disk_sample() const {
+    point3 defocus_disk_sample() const
+    {
         // Returns a random point in the camera defocus disk.
         auto p = random_in_unit_disk();
         return center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
     }
 
-    [[nodiscard]] color ray_color(const ray& r, int depth, hittable& world) {
+    [[nodiscard]] color ray_color(const ray &r, int depth, hittable &world)
+    {
         // If we pass ray bounce limit, no more light gathered.
-        if (depth <= 0) {
-            return {0,0,0};
+        if (depth <= 0)
+        {
+            return {0, 0, 0};
         }
 
         hit_record rec;
@@ -162,10 +230,10 @@ private:
         if (!rec.mat->scatter(r, rec, attenuation, scattered))
             return color_from_emission;
 
-        color color_from_scatter = attenuation * ray_color(scattered, depth-1, world);
+        color color_from_scatter = attenuation * ray_color(scattered, depth - 1, world);
 
         return color_from_emission + color_from_scatter;
     }
 };
 
-#endif //RAYTRACING_WEEKEND_CAMERA_H
+#endif // RAYTRACING_WEEKEND_CAMERA_H
--- src/main.cpp
+++ src/main.cpp
@@ -7,199 +7,204 @@
 #include "texture/texture.h"
 #include "scene/scene.h"
 
-void custom() {
+void custom()
+{
     scene world;
     auto t = mesh("../samples/cat/12221_Cat_v1_l3.obj"); // , {0, 22, -30}
     t.set_origin({0, 22, -30});
     t.rescale(0.2);
 
     auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
-    auto material_left   = make_shared<dielectric>(1.50);
+    auto material_left = make_shared<dielectric>(1.50);
     auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2), 0.0);
 
-    world.add_sphere(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-    world.add_sphere(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add_sphere(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.4, material_bubble));
-    world.add_sphere(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    world.add_sphere(make_shared<sphere>(point3(0.0, 0.0, -1.2), 0.5, material_center));
+    world.add_sphere(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add_sphere(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.4, material_bubble));
+    world.add_sphere(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
     world.add_obj(make_shared<mesh>(t));
 
     auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
-    world.add_sphere(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(checker)));
+    world.add_sphere(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
     world.build();
 
     camera cam;
 
-    cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 400;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
     cam.samples_per_pixel = 100;
-    cam.max_depth         = 3;
-    cam.background        = color(0.70, 0.80, 1.00);
+    cam.max_depth = 3;
+    cam.background = color(0.70, 0.80, 1.00);
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0.6;
-    cam.focus_dist    = 10.0;
+    cam.focus_dist = 10.0;
 
     cam.render(world);
 }
 
-void checkered_spheres() {
+void checkered_spheres()
+{
     scene world;
     auto checker = make_shared<checker_texture>(0.32, color(.2, .3, .1), color(.9, .9, .9));
 
-    world.add_sphere(make_shared<sphere>(point3(0,-10, 0), 10, make_shared<lambertian>(checker)));
+    world.add_sphere(make_shared<sphere>(point3(0, -10, 0), 10, make_shared<lambertian>(checker)));
     world.add_sphere(make_shared<sphere>(point3(0, 10, 0), 10, make_shared<lambertian>(checker)));
     world.build();
     camera cam;
 
-    cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 400;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
     cam.samples_per_pixel = 100;
-    cam.max_depth         = 3;
-    cam.background        = color(0.70, 0.80, 1.00);
+    cam.max_depth = 3;
+    cam.background = color(0.70, 0.80, 1.00);
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0;
 
     cam.render(world);
 }
 
-void perlin() {
+void perlin()
+{
     scene world;
 
     auto pertext = make_shared<noise_texture>(4);
-    world.add_sphere(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(pertext)));
-    world.add_sphere(make_shared<sphere>(point3(0,2,0), 2, make_shared<lambertian>(pertext)));
+    world.add_sphere(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+    world.add_sphere(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
     world.build();
 
     camera cam;
-    cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 400;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
     cam.samples_per_pixel = 100;
-    cam.max_depth         = 3;
-    cam.background        = color(0.70, 0.80, 1.00);
+    cam.max_depth = 3;
+    cam.background = color(0.70, 0.80, 1.00);
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0;
 
     cam.render(world);
 }
 
-//void quads() {
-//    hittable_list world;
+// void quads() {
+//     hittable_list world;
 //
-//    auto left_red     = make_shared<lambertian>(color(1.0, 0.2, 0.2));
-//    auto back_green   = make_shared<lambertian>(color(0.2, 1.0, 0.2));
-//    auto right_blue   = make_shared<lambertian>(color(0.2, 0.2, 1.0));
-//    auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
-//    auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
+//     auto left_red     = make_shared<lambertian>(color(1.0, 0.2, 0.2));
+//     auto back_green   = make_shared<lambertian>(color(0.2, 1.0, 0.2));
+//     auto right_blue   = make_shared<lambertian>(color(0.2, 0.2, 1.0));
+//     auto upper_orange = make_shared<lambertian>(color(1.0, 0.5, 0.0));
+//     auto lower_teal   = make_shared<lambertian>(color(0.2, 0.8, 0.8));
 //
-//    world.add(make_shared<quad>(point3(-3,-2, 5), vec3(0, 0,-4), vec3(0, 4, 0), left_red));
-//    world.add(make_shared<quad>(point3(-2,-2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
-//    world.add(make_shared<quad>(point3( 3,-2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
-//    world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
-//    world.add(make_shared<quad>(point3(-2,-3, 5), vec3(4, 0, 0), vec3(0, 0,-4), lower_teal));
+//     world.add(make_shared<quad>(point3(-3,-2, 5), vec3(0, 0,-4), vec3(0, 4, 0), left_red));
+//     world.add(make_shared<quad>(point3(-2,-2, 0), vec3(4, 0, 0), vec3(0, 4, 0), back_green));
+//     world.add(make_shared<quad>(point3( 3,-2, 1), vec3(0, 0, 4), vec3(0, 4, 0), right_blue));
+//     world.add(make_shared<quad>(point3(-2, 3, 1), vec3(4, 0, 0), vec3(0, 0, 4), upper_orange));
+//     world.add(make_shared<quad>(point3(-2,-3, 5), vec3(4, 0, 0), vec3(0, 0,-4), lower_teal));
 //
-//    camera cam;
+//     camera cam;
 //
-//    cam.aspect_ratio      = 1.0;
-//    cam.image_width       = 400;
-//    cam.samples_per_pixel = 100;
-//    cam.max_depth         = 50;
-//    cam.background        = color(0.70, 0.80, 1.00);
+//     cam.aspect_ratio      = 1.0;
+//     cam.image_width       = 400;
+//     cam.samples_per_pixel = 100;
+//     cam.max_depth         = 50;
+//     cam.background        = color(0.70, 0.80, 1.00);
 //
-//    cam.vfov     = 80;
-//    cam.lookfrom = point3(0,0,9);
-//    cam.lookat   = point3(0,0,0);
-//    cam.vup      = vec3(0,1,0);
+//     cam.vfov     = 80;
+//     cam.lookfrom = point3(0,0,9);
+//     cam.lookat   = point3(0,0,0);
+//     cam.vup      = vec3(0,1,0);
 //
-//    cam.defocus_angle = 0;
+//     cam.defocus_angle = 0;
 //
-//    cam.render(world);
-//}
+//     cam.render(world);
+// }
 
-void simple_light() {
+void simple_light()
+{
     scene world;
 
     auto pertext = make_shared<noise_texture>(4);
-    world.add_sphere(make_shared<sphere>(point3(0,-1000,0), 1000, make_shared<lambertian>(pertext)));
-    world.add_sphere(make_shared<sphere>(point3(0,2,0), 2, make_shared<lambertian>(pertext)));
+    world.add_sphere(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(pertext)));
+    world.add_sphere(make_shared<sphere>(point3(0, 2, 0), 2, make_shared<lambertian>(pertext)));
 
-    auto difflight = make_shared<diffuse_light>(color(0,0,4));
-    world.add_sphere(make_shared<sphere>(point3(0,7,0), 2, difflight));
+    auto difflight = make_shared<diffuse_light>(color(0, 0, 4));
+    world.add_sphere(make_shared<sphere>(point3(0, 7, 0), 2, difflight));
     auto material_bubble = make_shared<dielectric>(1.00 / 1.50);
-    world.add_sphere(make_shared<sphere>(point3(0,7,0),   2.5, material_bubble));
+    world.add_sphere(make_shared<sphere>(point3(0, 7, 0), 2.5, material_bubble));
     world.build();
     camera cam;
 
-    cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 400;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
     cam.samples_per_pixel = 100;
-    cam.max_depth         = 3;
-    cam.background        = color(0,0,0);
+    cam.max_depth = 3;
+    cam.background = color(0, 0, 0);
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(26,3,6);
-    cam.lookat   = point3(0,2,0);
-    cam.vup      = vec3(0,1,0);
+    cam.vfov = 20;
+    cam.lookfrom = point3(26, 3, 6);
+    cam.lookat = point3(0, 2, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0;
 
     cam.render(world);
 }
 
-//void cornell_box() {
-//    hittable_list world;
+// void cornell_box() {
+//     hittable_list world;
 //
-//    auto red   = make_shared<lambertian>(color(.65, .05, .05));
-//    auto white = make_shared<lambertian>(color(.73, .73, .73));
-//    auto green = make_shared<lambertian>(color(.12, .45, .15));
-//    auto light = make_shared<diffuse_light>(color(15, 15, 15));
+//     auto red   = make_shared<lambertian>(color(.65, .05, .05));
+//     auto white = make_shared<lambertian>(color(.73, .73, .73));
+//     auto green = make_shared<lambertian>(color(.12, .45, .15));
+//     auto light = make_shared<diffuse_light>(color(15, 15, 15));
 //
-//    world.add(make_shared<quad>(point3(555,0,0), vec3(0,555,0), vec3(0,0,555), green));
-//    world.add(make_shared<quad>(point3(0,0,0), vec3(0,555,0), vec3(0,0,555), red));
-//    world.add(make_shared<quad>(point3(343, 554, 332), vec3(-130,0,0), vec3(0,0,-105), light));
-//    world.add(make_shared<quad>(point3(0,0,0), vec3(555,0,0), vec3(0,0,555), white));
-//    world.add(make_shared<quad>(point3(555,555,555), vec3(-555,0,0), vec3(0,0,-555), white));
-//    world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white));
+//     world.add(make_shared<quad>(point3(555,0,0), vec3(0,555,0), vec3(0,0,555), green));
+//     world.add(make_shared<quad>(point3(0,0,0), vec3(0,555,0), vec3(0,0,555), red));
+//     world.add(make_shared<quad>(point3(343, 554, 332), vec3(-130,0,0), vec3(0,0,-105), light));
+//     world.add(make_shared<quad>(point3(0,0,0), vec3(555,0,0), vec3(0,0,555), white));
+//     world.add(make_shared<quad>(point3(555,555,555), vec3(-555,0,0), vec3(0,0,-555), white));
+//     world.add(make_shared<quad>(point3(0,0,555), vec3(555,0,0), vec3(0,555,0), white));
 //
-//    camera cam;
+//     camera cam;
 //
-//    cam.aspect_ratio      = 1.0;
-//    cam.image_width       = 600;
-//    cam.samples_per_pixel = 200;
-//    cam.max_depth         = 50;
-//    cam.background        = color(0,0,0);
+//     cam.aspect_ratio      = 1.0;
+//     cam.image_width       = 600;
+//     cam.samples_per_pixel = 200;
+//     cam.max_depth         = 50;
+//     cam.background        = color(0,0,0);
 //
-//    cam.vfov     = 40;
-//    cam.lookfrom = point3(278, 278, -800);
-//    cam.lookat   = point3(278, 278, 0);
-//    cam.vup      = vec3(0,1,0);
+//     cam.vfov     = 40;
+//     cam.lookfrom = point3(278, 278, -800);
+//     cam.lookat   = point3(278, 278, 0);
+//     cam.vup      = vec3(0,1,0);
 //
-//    cam.defocus_angle = 0;
+//     cam.defocus_angle = 0;
 //
-//    cam.render(world);
-//}
+//     cam.render(world);
+// }
 
-void custom2() {
+void custom2()
+{
     scene world;
     auto t = mesh("../samples/cat/12221_Cat_v1_l3.obj"); // , {0, 22, -30}
     t.set_origin({0, 22, -30});
     t.rescale(0.2);
 
-    auto difflight = make_shared<diffuse_light>(color(4,4,4));
+    auto difflight = make_shared<diffuse_light>(color(4, 4, 4));
     auto checker = make_shared<checker_texture>(0.32, color(.5, .0, .1), color(.9, .9, .9));
 
     world.add_sphere(make_shared<sphere>(point3(0, -1000, 0), 1000, make_shared<lambertian>(checker)));
@@ -207,68 +212,80 @@ void custom2() {
     world.add_sphere(make_shared<sphere>(point3(0, 30, 0), 2, difflight));
     world.build();
 
-
     camera cam;
 
-    cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 400;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 400;
     cam.samples_per_pixel = 100;
-    cam.max_depth         = 3;
-    cam.background        = color(0.0, 0.0, 0.0);
+    cam.max_depth = 3;
+    cam.background = color(0.0, 0.0, 0.0);
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0.6;
-    cam.focus_dist    = 10.0;
+    cam.focus_dist = 10.0;
 
     cam.render(world);
 }
 
-void custom3() {
+void custom3()
+{
     scene world;
     auto t = mesh("../samples/cat/12221_Cat_v1_l3.obj"); // , {0, 22, -30}
     t.set_origin({0, 0, 0});
     t.rescale(0.1);
 
-    auto difflight = make_shared<diffuse_light>(color(4,4,4));
+    auto difflight = make_shared<diffuse_light>(color(4, 4, 4));
     auto checker = make_shared<checker_texture>(0.32, color(.5, .0, .1), color(.9, .9, .9));
 
-//    world.add_sphere(make_shared<sphere>(point3(0, 50000, -30000), 1000, make_shared<lambertian>(checker)));
+    //    world.add_sphere(make_shared<sphere>(point3(0, 50000, -30000), 1000, make_shared<lambertian>(checker)));
     world.add_obj(make_shared<mesh>(t));
-//    world.add_sphere(make_shared<sphere>(point3(0, 30, -10), 2, difflight));
+    //    world.add_sphere(make_shared<sphere>(point3(0, 30, -10), 2, difflight));
     world.build();
-
 
     camera cam;
 
-    cam.aspect_ratio      = 16.0 / 9.0;
-    cam.image_width       = 1080;
+    cam.aspect_ratio = 16.0 / 9.0;
+    cam.image_width = 1080;
     cam.samples_per_pixel = 200;
-    cam.max_depth         = 3;
-    cam.background        = color(0.0, 1.0, 1.0);
+    cam.max_depth = 3;
+    cam.background = color(0.0, 1.0, 1.0);
 
-    cam.vfov     = 20;
-    cam.lookfrom = point3(13,2,3);
-    cam.lookat   = point3(0,0,0);
-    cam.vup      = vec3(0,1,0);
+    cam.vfov = 20;
+    cam.lookfrom = point3(13, 2, 3);
+    cam.lookat = point3(0, 0, 0);
+    cam.vup = vec3(0, 1, 0);
 
     cam.defocus_angle = 0.6;
-    cam.focus_dist    = 10.0;
+    cam.focus_dist = 10.0;
 
-    cam.render(world);
+    // cam.render(world);
+    cam.render_montecarlo(world);
 }
 
-int main() {
-    switch (7) {
-        case 1: custom();  break;
-        case 2: checkered_spheres(); break;
-        case 3: perlin(); break;
-//        case 4: quads(); break;
-        case 5:  simple_light(); break;
-//        case 6: cornell_box(); break;
-        case 7: custom3(); break;
+int main()
+{
+    switch (7)
+    {
+    case 1:
+        custom();
+        break;
+    case 2:
+        checkered_spheres();
+        break;
+    case 3:
+        perlin();
+        break;
+        //        case 4: quads(); break;
+    case 5:
+        simple_light();
+        break;
+        //        case 6: cornell_box(); break;
+    case 7:
+        custom3();
+        break;
     }
 }
